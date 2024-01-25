94f24ce22226d8a2db5cdef39c1ab693cdcfc2e6
Alvaro <17alma41@gmail.com>
2024-01-25 10:33:21 +0100

    update: asignacion de botones aleatorios

--- script.js
+++ script.js
@@ -11,21 +11,37 @@ async function getPkmn(numeroAleatorio) {
 async function mi_peticion(){
     //Sale un número aleatorio que genera la imagen de un pokemon
     const numeroAleatorio = generarNumeroAleatorio();
-    const pkmm = await getPkmn(numeroAleatorio);
-    const urlImagenFrontal = pkmm.sprites.front_default;
+    const numeroAleatorio1 = generarNumeroAleatorio();
+    const numeroAleatorio2 = generarNumeroAleatorio();
+    const numeroAleatorio3 = generarNumeroAleatorio();
+    
+    const pkmn = await getPkmn(numeroAleatorio);
+    const pkmn1 = await getPkmn(numeroAleatorio1);
+    const pkmn2 = await getPkmn(numeroAleatorio2);
+    const pkmn3 = await getPkmn(numeroAleatorio3);
 
+    const {front_default} = pkmn.sprites
+    const urlImagenFrontal = front_default
+    
     const imgPokemon = document.getElementById("imgPokemon");
     imgPokemon.src = urlImagenFrontal;
 
     //Accedo a la informacion de nombre de la API para que me cambie el nombre de pokemon en el botón
-    mostrarNombrePokemon(pkmm.name);
+    mostrarNombrePokemon(pkmn.name, pkmn1.name, pkmn2.name, pkmn3.name);
 }
 mi_peticion()
 
-function mostrarNombrePokemon(nombrePokemon) {
+function mostrarNombrePokemon(nombrePokemon, nombrePokemon1, nombrePokemon2, nombrePokemon3) {
     //Modifico la información del botón del HTML
     const btnObtenerPokemon = document.getElementById("btnObtenerPokemon");
-    btnObtenerPokemon.innerHTML = `${nombrePokemon}`;
+    const btnObtenerPokemon1 = document.getElementById("btnObtenerPokemon1");
+    const btnObtenerPokemon2 = document.getElementById("btnObtenerPokemon2");
+    const btnObtenerPokemon3 = document.getElementById("btnObtenerPokemon3");
+    
+    btnObtenerPokemon.value = `${nombrePokemon}`;
+    btnObtenerPokemon1.value = `${nombrePokemon1}`;
+    btnObtenerPokemon2.value = `${nombrePokemon2}`;
+    btnObtenerPokemon3.value = `${nombrePokemon3}`;
 }
 
 function generarNumeroAleatorio(){
